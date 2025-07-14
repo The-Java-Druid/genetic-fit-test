29ab4353b2a4df3595e897f5186742af58ba755c
Alied Perez Martinez <aliedperezmartinez@gmail.com>
2025-07-14 10:26:31 -0400

    Refactor TestLoader - Introduce `AbstractTestModelConstruct` as a base class for custom SnakeYAML constructors. - Rename constructor classes from `*Constructor` to `*Construct` - Move `Tag` constant definitions into the `TestConstructor` - Utilize streams and `TypeDescription` for more concise type registration. - Simplify the construction logic within each specific `*Construct` class by leveraging the new helper methods from `AbstractTestModelConstruct`.

--- app/src/main/java/com/geneticfittest/serialisation/TestLoader.java
+++ app/src/main/java/com/geneticfittest/serialisation/TestLoader.java
@@ -20,16 +20,12 @@ import org.yaml.snakeyaml.nodes.SequenceNode;
 import org.yaml.snakeyaml.nodes.Tag;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestLoader {
 
-    private static final Tag SECTION_TAG = new Tag(Section.class);
-    private static final Tag RESULTS_TAG = new Tag(Results.class);
-    private static final Tag QUESTION_TAG = new Tag(Question.class);
-    private static final Tag ANSWER_TAG = new Tag(Answer.class);
-    private static final Tag RESULT_RANGE_TAG = new Tag(ResultRange.class);
     private final Yaml yaml = new Yaml(new TestConstructor(new LoaderOptions()));
 
     public TestModel load(InputStream inputStream) {
@@ -38,57 +34,76 @@ public class TestLoader {
 
     private class TestConstructor extends Constructor {
 
+        private static final Tag SECTION_TAG = new Tag(Section.class);
+        private static final Tag QUESTION_TAG = new Tag(Question.class);
+        private static final Tag ANSWER_TAG = new Tag(Answer.class);
+        private static final Tag RESULTS_TAG = new Tag(Results.class);
+        private static final Tag RESULT_RANGE_TAG = new Tag(ResultRange.class);
+
         public TestConstructor(LoaderOptions loadingConfig) {
             super(TestModel.class, loadingConfig);
-            this.yamlClassConstructors.put(NodeId.mapping, new TestModelConstructor());
-            this.yamlConstructors.put(SECTION_TAG, new SectionConstructor());
-            this.yamlConstructors.put(QUESTION_TAG, new QuestionConstructor());
-            this.yamlConstructors.put(ANSWER_TAG, new AnswerConstructor());
-            this.yamlConstructors.put(RESULT_RANGE_TAG, new ResultRangeConstructor());
-            this.yamlConstructors.put(RESULTS_TAG, new ResultsConstructor());
+            this.yamlClassConstructors.put(NodeId.mapping, new TestModelConstruct());
+            this.yamlConstructors.put(SECTION_TAG, new SectionConstruct());
+            this.yamlConstructors.put(QUESTION_TAG, new QuestionConstruct());
+            this.yamlConstructors.put(ANSWER_TAG, new AnswerConstruct());
+            this.yamlConstructors.put(RESULTS_TAG, new ResultsConstruct());
+            this.yamlConstructors.put(RESULT_RANGE_TAG, new ResultRangeConstruct());
+            Stream.of(TestModel.class, Section.class, Question.class, Answer.class, Results.class, ResultRange.class)
+                .map(TypeDescription::new)
+                .forEach(this::addTypeDescription);
+        }
+        private class AbstractTestModelConstruct extends Constructor.ConstructMapping {
 
-            addTypeDescription(new TypeDescription(TestModel.class));
-            addTypeDescription(new TypeDescription(Section.class));
-            addTypeDescription(new TypeDescription(Question.class));
-            addTypeDescription(new TypeDescription(Answer.class));
-            addTypeDescription(new TypeDescription(Results.class));
-            addTypeDescription(new TypeDescription(ResultRange.class));
+            protected String getScalarValue(Node valueNode) {
+                return ((ScalarNode)valueNode).getValue();
+            }
+
+            protected List<Node> getSequenceValue(Node valueNode) {
+                return ((SequenceNode) valueNode).getValue();
+            }
+
+            protected List<NodeTuple> getMappingValue(Node node) {
+                return ((MappingNode) node).getValue();
+            }
+
+            protected <T> List<T> getChildren(NodeTuple tuple, Class<T> type, Tag tag) {
+                return getChildren(tuple.getValueNode(), type, tag);
+            }
+
+            protected <T> List<T> getChildren(Node node, Class<T> type, Tag tag) {
+                return getSequenceValue(node).stream()
+                    .map(n -> buildObject(n, type, tag))
+                    .collect(Collectors.toList());
+            }
+
+            protected <T> T buildObject(Node node, Class<T> type, Tag tag) {
+                node.setType(type);
+                node.setTag(tag);
+                node.setTwoStepsConstruction(false);
+                node.setUseClassConstructor(false);
+                return (T) constructObject(node);
+            }
+
         }
 
-        private class TestModelConstructor extends ConstructMapping {
+        private class TestModelConstruct extends AbstractTestModelConstruct {
 
             @Override
             public Object construct(Node node) {
                 node.setTwoStepsConstruction(false);
-                final MappingNode mappingNode = (MappingNode) node;
                 String title = null;
-                final List<Section> sections = new ArrayList<>();
+                List<Section> sections = null;
                 Results results = null;
-                for (NodeTuple tuple : mappingNode.getValue()) {
-                    final ScalarNode keyNode = (ScalarNode) tuple.getKeyNode();
-                    final String value = keyNode.getValue();
-                    final Node valueNode = tuple.getValueNode();
-                    switch (value) {
+                for (NodeTuple tuple : getMappingValue(node)) {
+                    switch (getScalarValue(tuple.getKeyNode())) {
                         case "title":
-                            final ScalarNode titleNode = (ScalarNode) valueNode;
-                            title = titleNode.getValue(); // Convert value node to String
+                            title = getScalarValue(tuple.getValueNode());
                             break;
                         case "sections":
-                            final SequenceNode sectionsNode = (SequenceNode) valueNode;
-                            for (Node sectionNode : sectionsNode.getValue()) {
-                                sectionNode.setType(Section.class);
-                                sectionNode.setTag(SECTION_TAG);
-                                sectionNode.setTwoStepsConstruction(false);
-                                sectionNode.setUseClassConstructor(false);
-                                sections.add((Section) constructObject(sectionNode)); // Recursively construct Section objects
-                            }
+                            sections = getChildren(tuple, Section.class, SECTION_TAG);
                             break;
                         case "results":
-                            valueNode.setType(Results.class);
-                            valueNode.setTag(RESULTS_TAG);
-                            valueNode.setTwoStepsConstruction(false);
-                            valueNode.setUseClassConstructor(false);
-                            results = (Results) constructObject(valueNode); // Recursively construct Results object
+                            results = buildObject(tuple.getValueNode(), Results.class, RESULTS_TAG);
                             break;
                     }
                 }
@@ -97,31 +112,19 @@ public class TestLoader {
 
         }
 
-        private class SectionConstructor extends ConstructMapping {
+        private class SectionConstruct extends AbstractTestModelConstruct {
 
             @Override
             public Object construct(Node node) {
-                final MappingNode mappingNode = (MappingNode) node;
                 String name = null;
-                final List<Question> questions = new ArrayList<>();
-                for (NodeTuple tuple : mappingNode.getValue()) {
-                    final ScalarNode keyNode = (ScalarNode) tuple.getKeyNode();
-                    final String value = keyNode.getValue();
-                    final Node valueNode = tuple.getValueNode();
-                    switch (value) {
+                List<Question> questions = null;
+                for (NodeTuple tuple : getMappingValue(node)) {
+                    switch (getScalarValue(tuple.getKeyNode())) {
                         case "name":
-                            final ScalarNode nameNode = (ScalarNode) valueNode;
-                            name = nameNode.getValue(); // Convert value node to String
+                            name = getScalarValue(tuple.getValueNode());
                             break;
                         case "questions":
-                            final SequenceNode questionsNode = (SequenceNode) valueNode;
-                            for (Node questionNode : questionsNode.getValue()) {
-                                questionNode.setType(Question.class);
-                                questionNode.setTag(QUESTION_TAG);
-                                questionNode.setTwoStepsConstruction(false);
-                                questionNode.setUseClassConstructor(false);
-                                questions.add((Question) constructObject(questionNode)); // Recursively construct Question objects
-                            }
+                            questions = getChildren(tuple, Question.class, QUESTION_TAG);
                             break;
                     }
                 }
@@ -129,56 +132,40 @@ public class TestLoader {
             }
         }
 
-        private class QuestionConstructor extends ConstructMapping {
+        private class QuestionConstruct extends AbstractTestModelConstruct {
+
             @Override
             public Object construct(Node node) {
-                final MappingNode mappingNode = (MappingNode) node;
                 String text = null;
-                final List<Answer> answers = new ArrayList<>();
-                for (NodeTuple tuple : mappingNode.getValue()) {
+                List<Answer> answers = null;
+                for (NodeTuple tuple : getMappingValue(node)) {
                     final ScalarNode keyNode = (ScalarNode) tuple.getKeyNode();
-                    final String value = keyNode.getValue();
-                    final Node valueNode = tuple.getValueNode();
-                    switch (value) {
+                    switch (keyNode.getValue()) {
                         case "text":
-                            final ScalarNode textNode = (ScalarNode) valueNode;
-                            text = textNode.getValue(); // Convert value node to String
+                            text = getScalarValue(tuple.getValueNode());
                             break;
                         case "answers":
-                            final SequenceNode answersNode = (SequenceNode) valueNode;
-                            for (Node answerNode : answersNode.getValue()) {
-                                answerNode.setType(Answer.class);
-                                answerNode.setTag(ANSWER_TAG);
-                                answerNode.setTwoStepsConstruction(false);
-                                answerNode.setUseClassConstructor(false);
-                                answers.add((Answer) constructObject(answerNode)); // Recursively construct Answer objects
-                            }
+                            answers = getChildren(tuple, Answer.class, ANSWER_TAG);
                             break;
-
                     }
                 }
                 return new Question(text, answers);
             }
         }
 
-        private class AnswerConstructor extends ConstructMapping {
+        private class AnswerConstruct extends AbstractTestModelConstruct {
+
             @Override
             public Object construct(Node node) {
-                final MappingNode mappingNode = (MappingNode) node;
                 String text = null;
                 int score = 0;
-                for (NodeTuple tuple : mappingNode.getValue()) {
-                    final ScalarNode keyNode = (ScalarNode) tuple.getKeyNode();
-                    final String value = keyNode.getValue();
-                    final Node valueNode = tuple.getValueNode();
-                    switch (value) {
+                for (NodeTuple tuple : getMappingValue(node)) {
+                    switch (getScalarValue(tuple.getKeyNode())) {
                         case "text":
-                            final ScalarNode textNode = (ScalarNode) valueNode;
-                            text = textNode.getValue(); // Convert value node to String
+                            text = getScalarValue(tuple.getValueNode());
                             break;
                         case "score":
-                            final ScalarNode scoreNode = (ScalarNode) valueNode;
-                            score = Integer.parseInt(scoreNode.getValue()); // Convert value node to String
+                            score = Integer.parseInt(getScalarValue(tuple.getValueNode()));
                             break;
                     }
                 }
@@ -186,51 +173,35 @@ public class TestLoader {
             }
         }
 
-        private class ResultRangeConstructor extends ConstructMapping {
+        private class ResultsConstruct extends AbstractTestModelConstruct {
+
             @Override
             public Object construct(Node node) {
-                final MappingNode mappingNode = (MappingNode) node;
+                return new Results(getChildren(node, ResultRange.class, RESULT_RANGE_TAG));
+            }
+
+        }
+
+        private class ResultRangeConstruct extends AbstractTestModelConstruct {
+            @Override
+            public Object construct(Node node) {
                 int min = 0;
                 int max = 0;
                 String text = null;
-                for (NodeTuple tuple : mappingNode.getValue()) {
-                    final ScalarNode keyNode = (ScalarNode) tuple.getKeyNode();
-                    final String value = keyNode.getValue();
-                    final Node valueNode = tuple.getValueNode();
-                    switch (value) {
+                for (NodeTuple tuple : getMappingValue(node)) {
+                    switch (getScalarValue(tuple.getKeyNode())) {
                         case "min":
-                            final ScalarNode minNode = (ScalarNode) valueNode;
-                            min = Integer.parseInt(minNode.getValue()); // Convert value node to String
+                            min = Integer.parseInt(getScalarValue(tuple.getValueNode()));
                             break;
                         case "max":
-                            final ScalarNode maxNode = (ScalarNode) valueNode;
-                            max = Integer.parseInt(maxNode.getValue()); // Convert value node to String
+                            max = Integer.parseInt(getScalarValue(tuple.getValueNode()));
                             break;
                         case "text":
-                            final ScalarNode textNode = (ScalarNode) valueNode;
-                            text = textNode.getValue(); // Convert value node to String
+                            text = getScalarValue(tuple.getValueNode());
                             break;
                     }
                 }
                 return new ResultRange(min, max, text);
-            }
-        }
-
-        private class ResultsConstructor extends ConstructMapping {
-
-            @Override
-            public Object construct(Node node) {
-                final SequenceNode sequenceNode = (SequenceNode) node;
-                final List<ResultRange> ranges = new ArrayList<>();
-                for (Node sequenceValue : sequenceNode.getValue()) {
-                    final MappingNode mappingNode = (MappingNode) sequenceValue;
-                    mappingNode.setType(ResultRange.class);
-                    mappingNode.setTag(RESULT_RANGE_TAG);
-                    mappingNode.setTwoStepsConstruction(false);
-                    mappingNode.setUseClassConstructor(false);
-                    ranges.add((ResultRange) constructObject(mappingNode)); // Recursively construct ResultRange objects
-                }
-                return new Results(ranges);
             }
         }
     }
