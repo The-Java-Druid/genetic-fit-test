de8cc5c12116e7bc323aa57bde552248bd822be5
Alied Perez Martinez <aliedperezmartinez@gmail.com>
2025-07-31 13:51:28 -0400

    extract anonymous class to static class

--- app/src/main/java/com/geneticfittest/MainActivity.java
+++ app/src/main/java/com/geneticfittest/MainActivity.java
@@ -18,45 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final TestLoader YAML = new TestLoader();
     private TestModel testModel;
-    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testModel = loadTestModelFromYaml();
-        viewPager = findViewById(R.id.viewPager);
+        final ViewPager2 viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new SectionPagerAdapter(this, testModel));
 // detect when user reaches the last page
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                int lastIndex = testModel.getSections().size() - 1;
-                if (position == lastIndex) {
-                    Toast.makeText(MainActivity.this, "Swipe again to finish", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
-                // detect when user tries to scroll past last page
-                if (state == ViewPager2.SCROLL_STATE_IDLE) {
-                    int current = viewPager.getCurrentItem();
-                    int lastIndex = testModel.getSections().size() - 1;
-                    if (current > lastIndex) {
-                        showFinalResult(); // user tried to go past last
-                    }
-                }
-            }
-        });
+        viewPager.registerOnPageChangeCallback(new MyOnPageChangeCallback(viewPager, testModel, this));
     }
 
     public void showFinalResult() {
-        final int totalScore = testModel.calculateTotalScore();
-        final String resultText = testModel.getResultText();
-        ResultActivity.start(this, totalScore, resultText);
+        ResultActivity.start(
+            this, testModel.calculateTotalScore(), testModel.getResultText());
     }
 
     private TestModel loadTestModelFromYaml() {
@@ -69,4 +45,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static class MyOnPageChangeCallback extends ViewPager2.OnPageChangeCallback {
+        private final ViewPager2 viewPager;
+        private final TestModel testModel;
+        private final MainActivity mainActivity;
+
+        public MyOnPageChangeCallback(ViewPager2 viewPager, TestModel testModel, MainActivity mainActivity) {
+            this.viewPager = viewPager;
+            this.testModel = testModel;
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            int lastIndex = testModel.getSections().size() - 1;
+            if (position == lastIndex) {
+                Toast.makeText(mainActivity, "Swipe again to finish", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            super.onPageScrollStateChanged(state);
+            // detect when user tries to scroll past last page
+            if (state == ViewPager2.SCROLL_STATE_IDLE) {
+                int current = viewPager.getCurrentItem();
+                int lastIndex = testModel.getSections().size() - 1;
+                if (current > lastIndex) {
+                    mainActivity.showFinalResult(); // user tried to go past last
+                }
+            }
+        }
+    }
 }
